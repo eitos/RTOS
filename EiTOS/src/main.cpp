a260d13e6332be7e8046668d98f5641a5929e648
Grzegorz Gajoch <gajoch@gmail.com>
2014-10-18 00:15:21 +0200

    Added project folder template and static code checker.

---
+++ EiTOS/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * EiTOS.cpp
+ *
+ * Created: 2014-10-17 23:49:26
+ *  Author: gregg
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void) {
+    while (1) {
+        // TODO(everyone) : Please write your application code
+    }
+}
