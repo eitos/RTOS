ea20343359851e77df26fc8edd097e0cf805e021
piotrva <piotrva@wp.pl>
2014-10-18 13:33:43 +0200

    Full Atmle Studio project  incl. debug setup with AVR Dragon & some junk code

---
+++ EiTOS/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ * Created: 2014-10-18 00:35:09
+ *  Author: Piotr
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(){
+	DDRB = (1<<PB0);
+	while(1){
+		PORTB ^= (1<<PB0);
+		_delay_ms(500);
+	}
+	return 0;
+}
