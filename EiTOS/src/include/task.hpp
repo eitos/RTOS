b96c1ae2e0357ab64e44d50ec1f9332fc6fc0749
Grzegorz Gajoch <gajoch@gmail.com>
2014-10-21 02:16:09 +0200

    cleanup

--- EiTOS/src/include/task.hpp
+++ EiTOS/src/include/task.hpp
@@ -1,29 +1,28 @@
 #ifndef __TASK_HPP__
 #define __TASK_HPP__
 
-#include <stdio.h>
 #include "../../port/TaskLowLevel.hpp"
 #include "../../include/EiTOSConfig.hpp"
 
 class TaskStruct_t : TaskLowLevel_t {
-public:
+ public:
     int val, id;
     uint8_t Norm() {
         return this->val;
     }
-private:
+ private:
 };
 
 
 class SchedulerQueue_t {
-private:
+ private:
 	SchedulerQueue_t();
     TaskStruct_t TaskList[TaskQueueSize];
     uint8_t ActualSize;
-public:
+ public:
     void init();
     uint8_t size();
-    void push(TaskStruct_t & task);
+    void push(const TaskStruct_t & task);
     TaskStruct_t & front();
     void pop();
 };
--- EiTOS/src/task.cpp
+++ EiTOS/src/task.cpp
@@ -10,17 +10,18 @@ uint8_t SchedulerQueue_t::size() {
     return this->ActualSize;
 }
 
-void SchedulerQueue_t::push(TaskStruct_t & task) {
+void SchedulerQueue_t::push(const TaskStruct_t & task) {
     int8_t InsertionPosition = -1;
-    for(int8_t i = 0; i < this->size(); ++i) {
-        if( this->TaskList[i].Norm()>= task.Norm() ) {
+    for (int8_t i = 0; i < this->size(); ++i) {
+        if ( this->TaskList[i].Norm() >= task.Norm() ) {
             InsertionPosition = i;
             break;
         }
     }
-    if( InsertionPosition == -1 )
+    if ( InsertionPosition == -1 ) {
         InsertionPosition = this->size();
-    for(int8_t i = this->size()-1; i >= InsertionPosition; --i) {
+    }
+    for (int8_t i = this->size()-1; i >= InsertionPosition; --i) {
         this->TaskList[i+1] = this->TaskList[i];
     }
     this->TaskList[InsertionPosition] = task;
