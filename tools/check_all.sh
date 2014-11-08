cb9c493d8f3f19fd6d96261067114c3a0df6d062
Grzegorz Gajoch <gajoch@gmail.com>
2014-11-08 13:14:36 +0100

    Linter fixup for travis.

--- tools/check_all.sh
+++ tools/check_all.sh
@@ -1,5 +1,6 @@
-find ../EiTOS -iname "*.cpp" -exec bash check_file.sh $1 {} \;
-find ../EiTOS -iname "*.c" -exec bash check_file.sh $1 {} \;
+TERM=zsh
+find ../EiTOS -iname "*.cpp" -print | xargs --no-run-if-empty -n 1 $TERM check_file.sh
+find ../EiTOS -iname "*.c" -print0 | xargs --no-run-if-empty -n 1 $TERM check_file.sh
 
-find ../EiTOS -iname "*.hpp" -exec bash check_file.sh $1 {} \;
-find ../EiTOS -iname "*.h" -exec bash check_file.sh $1 {} \;
+find ../EiTOS -iname "*.hpp" -print | xargs --no-run-if-empty -n 1 $TERM check_file.sh
+find ../EiTOS -iname "*.h" -print | xargs --no-run-if-empty -n 1 $TERM check_file.sh
--- tools/check_file.sh
+++ tools/check_file.sh
@@ -1,5 +1,1 @@
 python cpplint.py --extensions=hpp,cpp,h,c --filter=-legal/copyright,-whitespace/tab,-readability/casting $1
-
-
-
-#-o -iname "*.c" -o -iname "*.h" -o -iname "*.hpp" -print #
