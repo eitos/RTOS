0fd48be8b3004c2a0b6381ccdb317a0d2741ae28
Grzegorz Gajoch <gajoch@gmail.com>
2014-11-08 13:16:56 +0100

    Bad terminal.

--- tools/check_all.sh
+++ tools/check_all.sh
@@ -1,4 +1,4 @@
-TERM=zsh
+TERM=bash
 find ../EiTOS -iname "*.cpp" -print | xargs --no-run-if-empty -n 1 $TERM check_file.sh
 find ../EiTOS -iname "*.c" -print0 | xargs --no-run-if-empty -n 1 $TERM check_file.sh
 
