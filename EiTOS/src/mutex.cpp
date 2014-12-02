4d86a6fb2350d90fb3517b190872ea0f53631e9f
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-02 10:24:45 +0100

    mutex basic implementation - to test.

--- EiTOS/src/mutex.cpp
+++ EiTOS/src/mutex.cpp
@@ -1,7 +1,12 @@
+#include <inttypes.h>
+
 #include "mutex.hpp"
-#include "task"
+#include "task.hpp"
+#include "EiTOSConfig.hpp"
+#include "port/port.hpp"
 
 uint8_t MutexUsed = 0;
+uint8_t MutexTaken[MAX_MUTEX] = {false};
 
 mutex::mutex() {
     MutexUsed++;
@@ -9,15 +14,28 @@ mutex::mutex() {
 }
 
 void mutex::give() {
-    for (uint8_t i = 0; i < TaskQueue->size(); i++) {
+    if ( MutexTaken[this->nr] == false )
+        return;
+    MutexTaken[this->nr] = false;
+    for (uint8_t i = 0; i < TaskQueue.size(); i++) {
         if ( TaskQueue[i].blockingMutexNr == this->nr ) {
-            // TODO(rexina): problem z usuwaniem z listy
+            TaskStruct_t taskRemoved = TaskQueue[i];
+            taskRemoved.blockingMutexNr = 0;
+            TaskQueue.remove(i);
+            TaskQueue.push(taskRemoved);
         }
     }
+    TriggerSysTick();
 }
 
 void mutex::take() {
+    MutexTaken[this->nr] = true;
+    TaskStruct_t task = TaskQueue.front();
+    TaskQueue.pop();
+    task.blockingMutexNr = this->nr;
+    TaskQueue.push(task);
+    TriggerSysTick();
 }
 
-void mutex::take(uint32_t timeout) {
+bool mutex::take(uint32_t timeout) {
 }
