ad81e7e2d8f89bf058a1d66887c7b883c8b7ff02
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-02 11:59:42 +0100

    extracting task operating functions in mutex struct

--- EiTOS/src/mutex.cpp
+++ EiTOS/src/mutex.cpp
@@ -13,27 +13,35 @@ mutex::mutex() {
     this->nr = MutexUsed;
 }
 
-void mutex::give() {
-    if ( MutexTaken[this->nr] == false )
-        return;
-    MutexTaken[this->nr] = false;
+static inline void unlockTasksByMutex(uint8_t mutexNr) {
     for (uint8_t i = 0; i < TaskQueue.size(); i++) {
-        if ( TaskQueue[i].blockingMutexNr == this->nr ) {
+        if ( TaskQueue[i].blockingMutexNr == mutexNr ) {
             TaskStruct_t taskRemoved = TaskQueue[i];
             taskRemoved.blockingMutexNr = 0;
             TaskQueue.remove(i);
             TaskQueue.push(taskRemoved);
         }
     }
+}
+
+void mutex::give() {
+    if ( MutexTaken[this->nr] == false )
+        return;
+    MutexTaken[this->nr] = false;
+    unlockTasksByMutex(this->nr);
     TriggerSysTick();
+}
+
+static inline void lockTaskByMutex(uint8_t mutexNr) {
+    TaskStruct_t task = TaskQueue.front();
+    TaskQueue.pop();
+    task.blockingMutexNr = mutexNr;
+    TaskQueue.push(task);
 }
 
 void mutex::take() {
     MutexTaken[this->nr] = true;
-    TaskStruct_t task = TaskQueue.front();
-    TaskQueue.pop();
-    task.blockingMutexNr = this->nr;
-    TaskQueue.push(task);
+    lockTaskByMutex(this->nr);
     TriggerSysTick();
 }
 
