3608605a77d6d31cc0d46d0dc54086644abb33ba
piotrva <piotrva@wp.pl>
2014-12-03 19:35:43 +0100

    task states added, ActualTaskKill added

--- EiTOS/src/OS.cpp
+++ EiTOS/src/OS.cpp
@@ -23,6 +23,7 @@ void sys::taskCreate(TaskHandler_t taskHandler,
     TaskStruct_t task;
     task.priority = priority;
     task.lowLevel = TaskAllocate(taskHandler, StackSize);
+	task.TaskState = TASK_S_RUNNING;
     TaskQueue.push(task);
 }
 
@@ -33,4 +34,33 @@ void sys::ProcSysTick() {
     ActrualRunningTaskStruct = TaskQueue.front();
     TaskQueue.pop();
     ContextSet(&ActrualRunningTaskStruct.lowLevel);
+}
+
+void sys::ActualTaskKill(){
+	// find task located lowest in the ram
+	/*
+	uint16_t LowestRamAddr = RAMEND;
+	for(uint8_t i = 0 ; i < TaskQueue.size() ; i++){
+		uint16_t RamAddr = TaskGetHeap(&(TaskQueue[i]).lowLevel);
+		if(RamAddr<LowestRamAddr)LowestRamAddr=RamAddr;
+	}
+	
+	// deallocate ram for deleted task
+	uint16_t BytesMoved=TaskDeallocate(&ActrualRunningTaskStruct.lowLevel,LowestRamAddr);
+	
+	for(uint8_t i = 0 ; i < TaskQueue.size() ; i++){
+		TaskGetHeap(&(TaskQueue[i]).lowLevel);
+		here we sholuld change all lowLevel.StackStart-=BytesMoved;
+	}
+	*/
+	// run next task - not saving actual task will cause it to be removed
+	ActrualRunningTaskStruct = TaskQueue.front();
+	TaskQueue.pop();
+	ContextSet(&ActrualRunningTaskStruct.lowLevel);
+
+	// Now we will execute this task (jump to first task)
+	ExecutePendingTask();  // This will also enable interrupts
+
+	// This will never execute, aber zicher ist zicher!
+	while (1) {}
 }
