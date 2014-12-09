2f9151575a35f26609756965b6980068b5a79d79
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-09 21:58:49 +0100

    main.cpp now tests mutexes.

--- EiTOS/main.cpp
+++ EiTOS/main.cpp
@@ -2,19 +2,22 @@
 #include <util/delay.h>
 #include <OS.hpp>
 #include <avr/interrupt.h>
-#include "drivers/serial.hpp"
+
+#include "../drivers/serial.hpp"
 
 Serial serial;
 
-mutex muteks_01;
+mutex mutex1;
+mutex serialMutex;
 
 void Task1() {
+    static uint8_t count = 0;
     while (1) {
-        static uint8_t i = 0;
         PORTB ^= (1 << PB2);
-        _delay_ms(50);
-        serial.printf("Test %d\r\n", i);
-        i++;
+        
+        serialMutex.take();
+        serial.printf("Test %d\r\n", count++);
+        serialMutex.give();
     }
 }
 
@@ -32,19 +35,58 @@ void Task3() {
     }
 }
 
+void Task4() {
+    mutex1.take();
+    for(uint8_t i = 0; i < 10; ++i) {
+        serialMutex.take();
+        serial.printf("mutex taken %d!\n\r",i);
+        serialMutex.give();
+        
+        PORTB ^= (1 << PB0);
+        _delay_ms(200);
+    }
+    mutex1.take(); //can't take second time - will halt here!
+    while(1) {
+        serialMutex.take();
+        serial.printf("error !\n\r");
+        serialMutex.give();
+    }
+}
+
+
+void mainTask() {
+    mutex1.take();
+    serialMutex.give();
+    sys::taskCreate(&Task1, 1, 0xFF);
+    sys::taskCreate(&Task2, 1, 0x20);
+    sys::taskCreate(&Task3, 1, 0x20);
+    sys::taskCreate(&Task4, 2, 0x20);
+    
+    for(uint8_t i = 20; i > 0; --i) {
+        serialMutex.take();
+        serial.printf("Waiting... %d\r\n", i);
+        serialMutex.give();
+        
+        _delay_ms(50);
+    }
+    mutex1.give(); //will unlock Task4 - with higer priority
+    
+    while(1) {
+        serialMutex.take();
+        serial.printf("idle task\n\r");
+        serialMutex.give();
+    }
+}
+
 int main() {
-    // TP ONLY BEGIN
-    serial.init(9600U);
+    serial.init(115200U);
+    serial.printf("START\n\r");
     DDRB = (1 << PB0)|(1 << PB1)|(1 << PB2);
     PORTB = 0;
 
-    sys::taskCreate(&Task1, 0, 0xFF);
-    sys::taskCreate(&Task2, 0, 0x20);
-    sys::taskCreate(&Task3, 0, 0x20);
-
-
-    // TP ONLY END
-
+    sys::taskCreate(&mainTask, 1, 0xFF);
+    
+    serial.printf("System BOOT\n\r");
     sys::boot();
 
     return 0;
