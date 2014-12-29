894b3e112698da4adb35800ede569da49cd7330f
piotrva <piotrva@wp.pl>
2014-12-30 00:41:54 +0100

    cleanup for da Travis

--- EiTOS/main.cpp
+++ EiTOS/main.cpp
@@ -10,15 +10,15 @@ Serial serial;
 mutex mutex1;
 mutex serialMutex;
 
-void Task1(){
-    while(1){
+void Task1() {
+    while (1) {
         _delay_ms(250);
         PORTB ^= (1 << PB0)|(1 << PB1)|(1 << PB2);
     }
 }
 
-void Task2(){
-    while(1){
+void Task2() {
+    while (1) {
         serial.printf("JAM ODBLOKOWAN!\r\n");
         mutex1.take();
     }
@@ -29,11 +29,11 @@ int main() {
     serial.printf("START\n\r");
     DDRB = (1 << PB0)|(1 << PB1)|(1 << PB2);
     PORTB = 0;
-    
-    TCCR1B |= (1 << WGM12)|(1 << CS12)|(1 << CS10);//CTC, presc 1024
-    OCR1A = 15625-1;//interrupt 1s
+
+    TCCR1B |= (1 << WGM12)|(1 << CS12)|(1 << CS10);  // CTC, presc 1024
+    OCR1A = 15625-1;  // interrupt 1s
     TIMSK1 |= (1 << OCIE1A);
-    
+
     mutex1.take();
     sys::taskCreate(&Task1, 1, 0xFF);
     sys::taskCreate(&Task2, 1, 0xFF);
@@ -44,6 +44,6 @@ int main() {
     return 0;
 }
 
-ISR(TIMER1_COMPA_vect){
+ISR(TIMER1_COMPA_vect) {
     mutex1.give();
 }
